b32be366be11387d59bfa3da5e8056477c36fac6
John Leano <43891947+Duckybobman@users.noreply.github.com>
2018-10-08 11:52:16 -0700

    Created ar1 to ar3

--- Main.java
+++ Main.java
@@ -30,21 +30,45 @@ class Main
          *          call this array ar1.  Print it out below.
          */
         System.out.println("*** PRINTING ar1 *");
-        
+        int[] ar1 = new int[n];
+        for (int i = 0; i<ar1.length; i++)
+        {
+            ar1[i]=i;
+        }
+        for (int i = 0; i<ar1.length; i++)
+        {
+            System.out.println("ar["+i+"] = "+ar1[i]);
+        }
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
-        
+        System.out.println("*** PRINTINT ar2 *");
+        int[] ar2 = new int[n];
+        for (int i = 0; i<ar1.length; i++)
+        {
+            ar2[i]=i;
+        }
+        for (int i = 0; i<ar1.length; i++)
+        {
+            System.out.println("ar["+i+"] = "+ar2[i]);
+        }
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
-        
+        System.out.println("*** PRINTING TASK 3 * ");
+        for (int i = 0; i<ar1.length; i++)
+        {
+            ar1[i]=i+1;
+        }
+        for (int i = 0; i<ar1.length; i++)
+        {
+            System.out.println("ar["+i+"] = "+ar1[i]);
+        }
 
-        
-        
+         
         /*
          * task 4. Create a new array called ar3.
          *         Copy the elements of ar1 into ar3.  Then do it again
@@ -52,6 +76,7 @@ class Main
          *         ar1: 1 2 3
          *         ar3: 1 2 3 0 1 2 3
          */
+        int[] ar3 = new int[n];
         
         
         /*
